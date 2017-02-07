944b0f7aa5cfe48fb84eca27ee177b8e1af174e1
Steve Villella <s.villella@live.com>
2017-02-07 01:17:07 -0500

    create users controller with actions new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'users/create'
+
   resources :products
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
