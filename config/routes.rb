ab93546571a108962bf77c27753ff66fb8d34a65
Steve Villella <s.villella@live.com>
2017-02-07 01:31:04 -0500

    add actions to users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
-  get 'users/create'
-
   resources :products
+  resources :users only: [:new, :create]
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
